b552d18e50d0895dbbc9568ee0f430d32b296561
Jean Amadeu <jean.amadeu2@gmail.com>
2024-11-15 11:34:09 -0300

    test: add tests for list users endpoint

--- services/user/handle_test.go
+++ services/user/handle_test.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"encoding/json"
 	"errors"
 	"gorm.io/gorm"
 	"net/http"
@@ -14,7 +15,24 @@ import (
 )
 
 var today = time.Now()
-var formatedDate = today.Format(time.RFC3339Nano)
+var user = schemas.User{
+	Model: gorm.Model{
+		ID:        1,
+		CreatedAt: today,
+		UpdatedAt: today,
+	},
+	Name:     "Test",
+	Document: "12345678901",
+	Email:    "test@test.com",
+}
+
+func getUserString() string {
+	b, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
 
 func TestUserHandlers(t *testing.T) {
 	userRepo := &mockUserRepository{}
@@ -31,16 +49,7 @@ func TestUserHandlers(t *testing.T) {
 		router.ServeHTTP(w, req)
 
 		expectedBody := "{" +
-			"\"data\":{" +
-			"\"ID\":1," +
-			"\"CreatedAt\":\"" + formatedDate + "\"," +
-			"\"UpdatedAt\":\"" + formatedDate + "\"," +
-			"\"DeletedAt\":null," +
-			"\"Name\":\"Test\"," +
-			"\"Document\":\"12345678901\"," +
-			"\"Email\":\"test@test.com\"," +
-			"\"AccountID\":0" +
-			"}," +
+			"\"data\":" + getUserString() + "," +
 			"\"message\":\"operation from handler: find-user-by-id successfull\"" +
 			"}"
 
@@ -62,29 +71,34 @@ func TestUserHandlers(t *testing.T) {
 		assert.Equal(t, expectedResponseBody, w.Body.String())
 	})
 
+	t.Run("should handle return a list of users", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", "/api/v1/users", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+		expectedResponseBody := "{\"data\":[" + getUserString() + "]," +
+			"\"message\":\"operation from handler: list-users successfull\"}"
+		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
 }
 
 type mockUserRepository struct{}
 
 func (m *mockUserRepository) FindById(id string) (*schemas.User, error) {
 	if id == "1" {
-		return &schemas.User{
-			Model: gorm.Model{
-				ID:        1,
-				CreatedAt: today,
-				UpdatedAt: today,
-			},
-			Name:     "Test",
-			Document: "12345678901",
-			Email:    "test@test.com",
-		}, nil
+		return &user, nil
 	} else {
 		return nil, errors.New("user not found")
 	}
 }
 
 func (m *mockUserRepository) ListUsers() (*[]schemas.User, error) {
-	return &[]schemas.User{}, nil
+	listUser := []schemas.User{user}
+	return &listUser, nil
 }
 func (m *mockUserRepository) Create(user *schemas.User) (schemas.User, error) {
 	return schemas.User{}, nil
