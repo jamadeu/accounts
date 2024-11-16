65b0f42df6b3db57570f9a9cc858b7c259ee58ba
Jean Amadeu <jean.amadeu2@gmail.com>
2024-11-16 09:57:53 -0300

    test: add test for create user endpoint

--- services/user/handle_test.go
+++ services/user/handle_test.go
@@ -1,13 +1,15 @@
 package user
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
-	"gorm.io/gorm"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jamadeu/accounts/schemas"
@@ -15,7 +17,7 @@ import (
 )
 
 var today = time.Now()
-var user = schemas.User{
+var userTest = schemas.User{
 	Model: gorm.Model{
 		ID:        1,
 		CreatedAt: today,
@@ -26,8 +28,8 @@ var user = schemas.User{
 	Email:    "test@test.com",
 }
 
-func getUserString() string {
-	b, err := json.Marshal(user)
+func getUserTestString() string {
+	b, err := json.Marshal(userTest)
 	if err != nil {
 		panic(err)
 	}
@@ -49,11 +51,11 @@ func TestUserHandlers(t *testing.T) {
 		router.ServeHTTP(w, req)
 
 		expectedBody := "{" +
-			"\"data\":" + getUserString() + "," +
+			"\"data\":" + getUserTestString() + "," +
 			"\"message\":\"operation from handler: find-user-by-id successfull\"" +
 			"}"
 
-		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, http.StatusOK, w.Code)
 		assert.NotEmpty(t, w.Body.String())
 		assert.Equal(t, expectedBody, w.Body.String())
 	})
@@ -67,7 +69,7 @@ func TestUserHandlers(t *testing.T) {
 		router.ServeHTTP(w, req)
 
 		expectedResponseBody := "{\"errorCode\":404,\"message\":\"user with id: 2 not found\"}"
-		assert.Equal(t, 404, w.Code)
+		assert.Equal(t, http.StatusNotFound, w.Code)
 		assert.Equal(t, expectedResponseBody, w.Body.String())
 	})
 
@@ -78,9 +80,140 @@ func TestUserHandlers(t *testing.T) {
 			t.Fatal(err)
 		}
 		router.ServeHTTP(w, req)
-		expectedResponseBody := "{\"data\":[" + getUserString() + "]," +
+
+		expectedResponseBody := "{\"data\":[" + getUserTestString() + "]," +
 			"\"message\":\"operation from handler: list-users successfull\"}"
-		assert.Equal(t, 200, w.Code)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return created user", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		payload := CreateUserRequest{
+			Name:     userTest.Name,
+			Document: userTest.Document,
+			Email:    userTest.Email,
+		}
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"data\":" + getUserTestString() + "," +
+			"\"message\":\"operation from handler: create-user successfull\"}"
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return 400 when request boddy is empty", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		b, err := json.Marshal("{}")
+		if err != nil {
+			t.Fatal(err)
+		}
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"errorCode\":400,\"message\":\"reqest body is empty or malformed\"}"
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return 400 when name is empty", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		payload := CreateUserRequest{
+			Document: userTest.Document,
+			Email:    userTest.Email,
+		}
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"errorCode\":400,\"message\":\"param: name (type: string) is required\"}"
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return 400 when document is empty", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		payload := CreateUserRequest{
+			Name:  userTest.Name,
+			Email: userTest.Email,
+		}
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"errorCode\":400,\"message\":\"param: document (type: string) is required\"}"
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return 400 when email is empty", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		payload := CreateUserRequest{
+			Name:     userTest.Name,
+			Document: userTest.Document,
+		}
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"errorCode\":400,\"message\":\"param: email (type: string) is required\"}"
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.Equal(t, expectedResponseBody, w.Body.String())
+	})
+
+	t.Run("should return 400 when email is invalid", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		payload := CreateUserRequest{
+			Name:     userTest.Name,
+			Document: userTest.Document,
+			Email:    "invalid email",
+		}
+		b, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest("POST", "/api/v1/user", bytes.NewBuffer(b))
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(w, req)
+
+		expectedResponseBody := "{\"errorCode\":400,\"message\":\"param: email (type: string) is required\"}"
+		assert.Equal(t, http.StatusBadRequest, w.Code)
 		assert.Equal(t, expectedResponseBody, w.Body.String())
 	})
 
@@ -90,18 +223,18 @@ type mockUserRepository struct{}
 
 func (m *mockUserRepository) FindById(id string) (*schemas.User, error) {
 	if id == "1" {
-		return &user, nil
+		return &userTest, nil
 	} else {
 		return nil, errors.New("user not found")
 	}
 }
 
 func (m *mockUserRepository) ListUsers() (*[]schemas.User, error) {
-	listUser := []schemas.User{user}
+	listUser := []schemas.User{userTest}
 	return &listUser, nil
 }
 func (m *mockUserRepository) Create(user *schemas.User) (schemas.User, error) {
-	return schemas.User{}, nil
+	return userTest, nil
 }
 func (m *mockUserRepository) Update(user *schemas.User) error {
 	return nil
